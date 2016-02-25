e880db159249cda4c16ccf05dd30bdb03e54349d
Keith Shaw <github@keithws.net>
2016-02-25 02:01:28 -0800

    Added tests and updated code to pass
    * added test that the data emitted is X10Addresses and X10Commands.
    * added code to decode the X10 house code, unit code, and command from the received data.
    * added test that only one event is emitted even though there may more than 1 frames received.
    * added code to prevent duplicate events from being emitted for 576ms after a valid data frame is received, parsed, and emitted.

--- tests/test-w800.js
+++ tests/test-w800.js
@@ -35,10 +35,10 @@ describe("Receiver", function () {
 		serialPort = new SerialPort("/path/to/fake/usb");
 		receiver = new Receiver(serialPort, function () {
 			receiver.isReady.should.equal(true);
-			receiver.on("data", function (data) {
-			    should.exist(data);
-				data.should.deepEqual(new Buffer([0b00011110, 0b11100001, 0b00100110, 0b11011001]));
-				// data.should.deepEqual([new X10Address("A16"), new X10Command("OFF")]);
+			receiver.on("data", function (data, rawData) {
+				should.exist(data);
+				rawData.should.deepEqual(new Buffer([0b00011110, 0b11100001, 0b00100110, 0b11011001]));
+				data.should.deepEqual([new X10Address("A16"), new X10Command("OFF")]);
 				done();
 			});
 		});
@@ -46,8 +46,44 @@ describe("Receiver", function () {
 		serialPort.emit("open");
 		serialPort.emit("data", new Buffer([0x29]));
 		// send A-16 OFF from the fake serial port
-		// the message is repeated six times when I press a remote button
 		serialPort.emit("data", new Buffer([0b01100100, 0b10011011, 0b01111000, 0b10000111]));
 	});
-});
 
+	it("decodes one to six repeated samples as just one X10 packet", function (done) {
+		serialPort = new SerialPort("/path/to/fake/usb");
+		receiver = new Receiver(serialPort, function () {
+			receiver.isReady.should.equal(true);
+			receiver.on("data", function (data, rawData) {
+				should.exist(data);
+				rawData.should.deepEqual(new Buffer([0b00000100, 0b11111011, 0b00000110, 0b11111001]));
+				data.should.deepEqual([new X10Address("A1"), new X10Command("OFF")]);
+				done();
+			});
+		});
+		// send 0x29 to indicate the W800 is online
+		serialPort.emit("open");
+		serialPort.emit("data", new Buffer([0x29]));
+		// send A-1 OFF from the fake serial port
+		// the message is repeated six times (over 576ms) when I press a remote button
+		serialPort.emit("data", new Buffer([0b01100000, 0b10011111, 0b00100000, 0b11011111]));
+		setTimeout(function () {
+			serialPort.emit("data", new Buffer([0b01100000, 0b10011111, 0b00100000, 0b11011111]));
+			setTimeout(function () {
+				serialPort.emit("data", new Buffer([0b01100000, 0b10011111, 0b00100000, 0b11011111]));
+				setTimeout(function () {
+					serialPort.emit("data", new Buffer([0b01100000, 0b10011111, 0b00100000, 0b11011111]));
+					setTimeout(function () {
+						serialPort.emit("data", new Buffer([0b01100000, 0b10011111, 0b00100000, 0b11011111]));
+						setTimeout(function () {
+							serialPort.emit("data", new Buffer([0b01100000, 0b10011111, 0b00100000, 0b11011111]));
+						}, 96);
+					}, 96);
+				}, 96);
+			}, 96);
+		}, 96);
+	});
+
+	// add more tests to ensure the data is decoded correctly
+	// based on samples from docs/w800rf32_protocol.txt
+
+});
